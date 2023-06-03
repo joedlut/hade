6df4d7fa5971f0230bd7f199c02402c2ea141b7e
joedlut <2532771263@qq.com>
2023-06-03 21:48:19 +0800

    day 2

--- framework/context.go
+++ framework/context.go
@@ -1,8 +1,13 @@
 package framework
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -51,6 +56,8 @@ func (ctx *Context) HasTimeout() bool {
 	return ctx.hasTimeout
 }
 
+//regin query url
+
 func (ctx *Context) QueryAll() map[string][]string {
 	if ctx.request != nil {
 		// func (u *URL) Query() Values
@@ -61,8 +68,138 @@ func (ctx *Context) QueryAll() map[string][]string {
 		//Values maps a string key to a list of values. It is typically used for query parameters and form values.
 		//Unlike in the http.Header map, the keys in a Values map are case-sensitive.
 		return map[string][]string(ctx.request.URL.Query())
+		//return ctx.request.URL.Query()
+	}
+	return map[string][]string{}
+}
+
+func (ctx *Context) QueryInt(key string, def int) int {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		length := len(vals)
+		if length > 0 {
+			//返回最后一个值
+			intval, err := strconv.Atoi(vals[length-1])
+			if err != nil {
+				return def
+			}
+			return intval
+		}
 	}
 	return def
+}
+
+func (ctx *Context) QueryString(key string, def string) string {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		length := len(vals)
+		if length > 0 {
+			//返回最后一个值
+			return vals[length-1]
+		}
+	}
+	return def
+}
+
+func (ctx *Context) QueryArray(key string, def []string) []string {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		return vals
+	}
+	return def
+}
+
+//region form post
+
+func (ctx *Context) FormAll() map[string][]string {
+	if ctx.request != nil {
+		return map[string][]string(ctx.request.PostForm)
+	}
+	return map[string][]string{}
+}
+
+func (ctx *Context) FormInt(key string, def int) int {
+	params := ctx.FormAll()
+	if vals, ok := params[key]; ok {
+		length := len(vals)
+		if length > 0 {
+			//返回最后一个值
+			intval, err := strconv.Atoi(vals[length-1])
+			if err != nil {
+				return def
+			}
+			return intval
+		}
+	}
+	return def
+}
+
+func (ctx *Context) FormString(key string, def string) string {
+	params := ctx.FormAll()
+	if vals, ok := params[key]; ok {
+		length := len(vals)
+		if length > 0 {
+			//返回最后一个值
+			return vals[length-1]
+		}
+	}
+	return def
+}
+
+func (ctx *Context) FormArray(key string, def []string) []string {
+	params := ctx.FormAll()
+	if vals, ok := params[key]; ok {
+		return vals
+	}
+	return def
+}
+
+// region application/json post
+func (ctx *Context) BindJson(obj interface{}) error {
+	if ctx.request != nil {
+		body, err := io.ReadAll(ctx.request.Body)
+		if err != nil {
+			return err
+		}
+		// ????
+		ctx.request.Body = io.NopCloser(bytes.NewBuffer(body))
+		err = json.Unmarshal(body, obj)
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("ctx.request empty")
+	}
+	return nil
+}
+
+// region response
+
+func (ctx *Context) Json(status int, obj interface{}) error {
+	if ctx.HasTimeout() {
+		return nil
+	}
+	ctx.responseWriter.Header().Set("Content-Type", "application/json")
+	ctx.responseWriter.WriteHeader(status)
+	byt, err := json.Marshal(obj)
+	if err != nil {
+		ctx.responseWriter.WriteHeader(500)
+		return err
+	}
+	ctx.responseWriter.Write(byt)
+	return nil
+}
+
+// ????
+
+func (ctx *Context) HTML(status int, obj interface{}, template string) error {
+	return nil
+}
+
+//?????
+
+func (ctx *Context) Text(status int, obj string) error {
+	return nil
 }
 
 // implement the context.Context
